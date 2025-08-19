29e0e5127c59fa7ec79da99218a2e3e15e31fdfb
ThembaTman0 <thembatman0@gmail.com>
2025-08-19 15:51:14 +0200

    v-0.0.1

--- accounts/src/main/java/com/bankwave/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/bankwave/accounts/controller/AccountsController.java
@@ -1,13 +1,23 @@
 package com.bankwave.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.bankwave.accounts.constants.AccountsConstants;
+import com.bankwave.accounts.dto.CustomerDto;
+import com.bankwave.accounts.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
-    @GetMapping("sayHello")
-
-    public String sayHello(){
-        return "Hello World";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 }
---
+++ accounts/src/main/java/com/bankwave/accounts/dto/ErrorResponseDto.java
@@ -0,0 +1,19 @@
+package com.bankwave.accounts.dto;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponseDto {
+
+    private String apiPath;
+
+    private HttpStatus errorCode;
+
+    private String errorMessage;
+
+    private LocalDateTime errorTime;
+}
