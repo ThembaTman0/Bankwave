d7370c4e6c28bee654b714d03c052c110b1c9885
ThembaTman0 <thembatman0@gmail.com>
2025-08-26 14:54:21 +0200

    v-0.0.6

--- accounts/src/main/java/com/bankwave/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/bankwave/accounts/dto/AccountsDto.java
@@ -1,14 +1,21 @@
 package com.bankwave.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "Account Number cannot be null or empty")
+    @Pattern(regexp = "(^|[0-9]{10})",message = "Account Number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account Type cannot be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch Address cannot be null or empty")
     private String branchAddress;
 
 
