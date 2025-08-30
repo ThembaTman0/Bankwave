8ea60cc306bb6fd60d17e4e0c8bd57e8299e793d
ThembaTman0 <thembatman0@gmail.com>
2025-08-30 22:58:54 +0200

    v-0.0.9

--- accounts/src/main/java/com/bankwave/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/bankwave/accounts/dto/CustomerDto.java
@@ -23,7 +23,7 @@ public class CustomerDto {
     private String name;
 
     @Schema(
-            description = "Email address of the customer", example = "tutor@bankwave.com"
+            description = "Email address of the customer", example = "thembatman0@gmail.com"
     )
     @NotEmpty(message = "Email address can not be a null or empty")
     @Email(message = "Email address should be a valid value")
